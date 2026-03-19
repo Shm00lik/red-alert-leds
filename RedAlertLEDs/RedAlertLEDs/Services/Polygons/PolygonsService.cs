7dc38b9467b39505bc17fa4c8c5e247ef66f0119
Yoav Levy <yoavlevy75@gmail.com>
2026-03-19 22:14:29 +0200

    it works but it is with a delay of about 15 seconds!!

--- RedAlertLEDs/RedAlertLEDs/Services/Polygons/PolygonsService.cs
+++ RedAlertLEDs/RedAlertLEDs/Services/Polygons/PolygonsService.cs
@@ -6,17 +6,26 @@ namespace RedAlertLEDs.Services.Polygons;
 public class PolygonsService
 {
     private List<string> _relevantPolygons = ["תל אביב"];
+    private DateTime _lastAlertTime = DateTime.Now;
 
     public event EventHandler<RelevantAlertEventArgs>? RelevantAlertReceived;
 
     public void OnAlertReceived(object? sender, AlertEventArgs e)
     {
-        var isRelevantAlert = _relevantPolygons.Any(p => e.Alert.Polygon == p);
+        if (e.Alert.Date <= _lastAlertTime)
+        {
+            return;
+        }
 
+        var isRelevantAlert = true; //_relevantPolygons.Any(p => e.Alert.Polygon == p);
+        
         if (!isRelevantAlert)
         {
             return;
         }
+        
+        _lastAlertTime = e.Alert.Date;
+
 
         OnRelevantAlertReceived(e.Alert);
     }
