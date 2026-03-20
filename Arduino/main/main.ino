09ea3e09d2d174d379b93b9cc9b6d997669ed425
Yoav Levy <yoavlevy75@gmail.com>
2026-03-20 21:46:41 +0200

    arduino

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -1,50 +1,32 @@
 #include <FastLED.h>
-
 #define NUM_LEDS 86
 #define DATA_PIN 13
 #define BRIGHTNESS 255
+#define NEW_DATA_HEADER 0xAA
+#define ACK_DATA 1
+#define BAUDRATE 115200
+#define DELAY_BETWEEN_UPDATES 1
 
 CRGB leds[NUM_LEDS];
 
-
 void setup() {
-  Serial.begin(9600);
-
+  Serial.begin(BAUDRATE);
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
-
   fill_solid(leds, NUM_LEDS, CRGB::Black);
-
+  pinMode(LED_BUILTIN, OUTPUT);
   FastLED.show();
 }
-
 
 void loop() {
-  delay(1000);
-
-  if (Serial.available() == 0) {
-    Serial.println("No data!");
-    return;
-  }
-
-  int inputColor = Serial.read();
-  Serial.println(inputColor);
-
-  CRGB color = getColor(inputColor);
-
-  fill_solid(leds, NUM_LEDS, color);
-  FastLED.show();
-}
-
-CRGB getColor(int inputColor) {
-  switch (inputColor) {
-    case 49:
-      return CRGB::OrangeRed;
-    case 50:
-      return CRGB::Red;
-    case 51:
-      return CRGB::Green;
-    default:
-      return CRGB::Black;
+  delay(DELAY_BETWEEN_UPDATES);
+  if (Serial.read() == NEW_DATA_HEADER) {
+    Serial.write(ACK_DATA);
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.readBytes((char*)leds, NUM_LEDS * 3);
+    FastLED.show();
+    Serial.write(ACK_DATA);
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
